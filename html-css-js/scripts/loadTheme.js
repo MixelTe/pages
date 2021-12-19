07d1a582d8f2fe3f07bed877c698d9f401ffc93f
Mike <63655075+MixelTe@users.noreply.github.com>
2021-12-19 13:24:04 +0300

    html-css-js updated

--- html-css-js/scripts/loadTheme.js
+++ html-css-js/scripts/loadTheme.js
@@ -12,9 +12,28 @@ function changeTheme() {
     if (lightTheme) {
         document.body.classList.add("lightTheme");
         document.body.classList.remove("darkTheme");
+        changeHeaderColor("#fff7e5")
     }
     else {
         document.body.classList.remove("lightTheme");
         document.body.classList.add("darkTheme");
+        changeHeaderColor("#1e1e1e")
+    }
+}
+
+function changeHeaderColor(color)
+{
+    let items = [
+        "theme-color",
+        "msapplication-navbutton-color",
+        "apple-mobile-web-app-status-bar-style",
+    ]
+    for (const item of items)
+    {
+        let el = document.querySelector(`meta[name="${item}"]`)
+        if (el)
+        {
+            lightTheme.setAttribute('content', color)
+        }
     }
 }
